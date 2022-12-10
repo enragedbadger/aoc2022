522e3d5eb2af94d3fdc310ec41ed324ea776515e
Pieter Cronje <pjcronje@gmail.com>
2022-12-10 14:21:50 +0000

    day10 part 2

--- part3.py
+++ part3.py
@@ -1,6 +1,17 @@
 # Advent of Code 2022 - part 3 of N
 
 
+def show_crt(crt):
+    for row in crt:
+        print("".join(row))
+
+
+def update_crt(crt, X, cycle):
+    x, y = int((cycle - 1) / 40), (cycle - 1) % 40
+    if y in [X - 1, X, X + 1]:
+        crt[x][y] = "#"
+
+
 def day10():
     with open("./inputs/day10", "r") as f:
         data = f.read().splitlines()
@@ -8,37 +19,46 @@ def day10():
     cycle = 0
     X = 1
     signal = []
-    for row in data:
-        match row.split(" "):
+
+    crt = [["." for __ in range(40)] for __ in range(6)]
+    show_crt(crt)
+
+    for d in data:
+        match d.split(" "):
             case ["noop"]:
                 cycle += 1
-                # print(f"start of {cycle=}")
-                # print("do nothing")
+                print(f"start of {cycle=}")
+                print("do nothing")
                 if not (cycle + 20) % 40:
                     signal.append(cycle * X)
-                    # print("interesting signal strength!")
-                # print(f"end of {cycle=}, {X=}")
+                    print("interesting signal strength!")
+                update_crt(crt, X, cycle)
+                print(f"end of {cycle=}, {X=}")
 
             case ["addx", val]:
                 val = int(val)
 
                 cycle += 1
-                # print(f"start of {cycle=}")
-                # print("wait")
+                print(f"start of {cycle=}. `addx {val}` started.")
+                print("wait")
                 if not (cycle + 20) % 40:
                     signal.append(cycle * X)
-                    # print("interesting signal strength!")
-                # print(f"end of {cycle=}, {X=}")
+                    print("interesting signal strength!")
+                update_crt(crt, X, cycle)
+                print(f"end of {cycle=}, {X=}")
 
                 cycle += 1
-                # print(f"start of {cycle=}")
-                # print("wait")
+                print(f"start of {cycle=}")
+                print("wait")
                 if not (cycle + 20) % 40:
                     signal.append(cycle * X)
-                    # print("interesting signal strength!")
-                X += val
-                # print(f"end of {cycle=}. `addx {val}` completed! {X=}")
+                    print("interesting signal strength!")
+                update_crt(crt, X, cycle)
 
+                X += val
+                print(f"end of {cycle=}. `addx {val}` completed! {X=}")
+
+    show_crt(crt)
     return sum(signal)
 
 
